f3bc65b80c5968bed09a8b2edcbc8cc59c8a53b1
noname <ungamingytb@gmail.com>
2024-01-03 19:02:58 +0100

    Added the regualation of the loop speed

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,13 @@ QTR qtr;
 int8_t motorLSpeed; // -255 to 255 ; 0 is STOP
 int8_t motorRSpeed; // -255 to 255 ; 0 is STOP
 
+float dt;
+uint32_t current_time, prev_time, elapsed_time = 0;
+uint32_t print_counter, serial_counter;
+
+void loopRate(int freq);
+void printLoopRate();
+
 void setup()
 {
     Serial.begin(115200); // Init the Serial communication
@@ -27,4 +34,27 @@ void setup()
 
 void loop()
 {
+    loopRate(2000); // Set the loop speed at 2000Hz
+}
+
+void loopRate(int freq)
+{
+    float invFreq = 1.0 / freq * 1000000.0;
+    unsigned long checker = micros();
+
+    // Sit in loop until appropriate time has passed
+    while (invFreq > (checker - current_time))
+    {
+        checker = micros();
+    }
+}
+
+void printLoopRate()
+{
+    if (current_time - print_counter > 10000)
+    {
+        print_counter = micros();
+        Serial.print(F("dt = "));
+        Serial.println(dt * 1000000.0);
+    }
 }
