c3d09f8fb46facdcfa08ffce99f573bb1c9a5ae5
noname <ungamingytb@gmail.com>
2024-01-03 18:57:46 +0100

    Created QTR lib

---
+++ include/QTR/QTR.cpp
@@ -0,0 +1,119 @@
+// Motor.cpp
+
+#include "QTR.h"
+#include "Arduino.h"
+
+QTR::QTR()
+{
+    // Default constructor
+}
+
+void QTR::init(uint8_t en, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, uint8_t d8)
+{
+    // Set the pins for the qtr sensor
+    this->en = en;
+    this->sensorPin[0] = d1;
+    this->sensorPin[1] = d2;
+    this->sensorPin[2] = d3;
+    this->sensorPin[3] = d4;
+    this->sensorPin[4] = d5;
+    this->sensorPin[5] = d6;
+    this->sensorPin[6] = d7;
+    this->sensorPin[7] = d8;
+
+    // Configure pins as INPUT and OUTPUTS
+    pinMode(en, OUTPUT);
+    pinMode(sensorPin[0], INPUT);
+    pinMode(sensorPin[1], INPUT);
+    pinMode(sensorPin[2], INPUT);
+    pinMode(sensorPin[3], INPUT);
+    pinMode(sensorPin[4], INPUT);
+    pinMode(sensorPin[5], INPUT);
+    pinMode(sensorPin[6], INPUT);
+    pinMode(sensorPin[7], INPUT);
+}
+
+uint16_t QTR::getValue(uint8_t sensor)
+{
+    if (sensor < 8)
+    {
+        return analogRead(sensorPin[sensor]);
+    }
+
+    return 0; // Handle invalid sensor number (out of range)
+}
+
+void QTR::calibrate(uint16_t time)
+{
+    whiteVal = analogRead(sensorPin[0]);
+    blackVal = analogRead(sensorPin[0]);
+
+    uint16_t currVal;
+
+    uint32_t c = 0;
+    while (c < time)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            currVal = analogRead(sensorPin[i]);
+
+            if (currVal < whiteVal)
+            {
+                whiteVal = currVal;
+            }
+            else if (currVal > blackVal)
+            {
+                blackVal = currVal;
+            }
+        }
+
+        treshold = (whiteVal + blackVal) / 2;
+
+        delay(1);
+        c++;
+    }
+}
+
+bool QTR::isBlack(uint8_t sensor)
+{
+    if (getValue(sensor) > treshold)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool QTR::isWhite(uint8_t sensor)
+{
+    if (getValue(sensor) < treshold)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void QTR::printInfos()
+{
+    if (qtrCurrentTime - qtrPrintCounter > 10000)
+    {
+        qtrPrintCounter = micros();
+        
+        Serial.printf("White: %d | Black: %d | Treshold: %d\n", whiteVal, blackVal, treshold);
+    }
+}
+
+void QTR::printArray()
+{
+    if (qtrCurrentTime - qtrPrintCounter > 10000)
+    {
+        qtrPrintCounter = micros();
+
+        Serial.printf("| %04d | %04d | %04d | %04d | %04d | %04d | %04d | %04d |\n", sensorPin[0], sensorPin[1], sensorPin[2], sensorPin[3], sensorPin[4], sensorPin[5], sensorPin[6], sensorPin[7]);
+    }
+}
---
+++ include/QTR/QTR.h
@@ -0,0 +1,90 @@
+// QTR.h
+
+#include <Arduino.h>
+
+#ifndef QTR_h
+#define QTR_h
+
+class QTR
+{
+public:
+    /**
+     * @brief Default constructor for the QTR class.
+     */
+    QTR();
+
+    /**
+     * @brief Function to set the pins for the QTR sensor
+     *
+     * @param en Pin number for the QTR enable
+     * @param d1 Pin number for the first sensor
+     * @param d2 Pin number for the second sensor
+     * @param d3 Pin number for the third sensor
+     * @param d4 Pin number for the fourth sensor
+     * @param d5 Pin number for the fifth sensor
+     * @param d6 Pin number for the sixth sensor
+     * @param d7 Pin number for the seventh sensor
+     * @param d8 Pin number for the eighth sensor
+     */
+    void init(uint8_t en, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, uint8_t d8);
+
+    /**
+     * @brief Function to get the value of the selected sensor
+     *
+     * @param sensor Selection of the sensor (0-7 ; 0 indexed)
+     * @return uint16_t Value of the sensor
+     */
+    uint16_t getValue(uint8_t sensor);
+
+    /**
+     * @brief Function to calibrate the QTR sensor
+     *
+     * @param time Time in ms for the calibration
+     */
+    void calibrate(uint16_t time);
+
+    /**
+     * @brief Function to know if the color under the sensor is black
+     * 
+     * @param sensor Selection of the sensor (0-7 ; 0 indexed)
+     * @return true True if the color is black
+     * @return false False if the color is white
+     */
+    bool isBlack(uint8_t sensor);
+
+    /**
+     * @brief Function to know if the color under the sensor is white
+     * 
+     * @param sensor Selection of the sensor (0-7 ; 0 indexed)
+     * @return true True if the color is white
+     * @return false False if the color is black
+     */
+    bool isWhite(uint8_t sensor);
+
+    /**
+     * @brief Function to print the value for the white, black
+     * and the treshold value (need Serial.begin()).
+     * 
+     */
+    void printInfos();
+
+    /**
+     * @brief Function to print the array of sensors. Prints
+     * 1 if black or 0 if white (need Serial.begin()).
+     * 
+     */
+    void printArray();
+
+private:
+    uint8_t en;
+    uint8_t sensorPin[8];
+
+    uint16_t whiteVal;
+    uint16_t blackVal;
+    uint16_t treshold;
+
+    uint32_t qtrCurrentTime;
+    uint32_t qtrPrintCounter;
+};
+
+#endif
